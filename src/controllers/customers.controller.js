9797240832c2a361c0fa4f9eca3ce74dab16dd3a
Lucas Luiz <lucas.luiz10498@gmail.com>
2022-12-11 13:19:18 -0300

    get customers finhesed

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,18 +1,53 @@
-import { connection } from "../database/db.js"
+import { connection } from "../database/db.js";
 
-export async function insertCustomers (req, res){
-    const {name, phone, cpf, birthday} = req.body;
-    console.log(typeof(name));
-    console.log(typeof(phone));
-    console.log(cpf.length);
-    const cpfExist = await connection.query(`SELECT * FROM customers WHERE cpf='${cpf}'`)
-if(cpfExist.rows.length > 0){
-   return res.sendStatus(409);
+export async function getCustomersCpf (req, res) {
+  const cpfQ = req.query.cpf;
+  console.log(cpfQ)
+  if(cpfQ){
+    try {
+      const buscaSem = await connection.query(`SELECT * FROM customers WHERE cpf LIKE '${cpfQ}%';`)
+      return res.send(buscaSem.rows);
+  } catch(err) {
+    return res.status(500).send(err);
+  }
+  }
+  try {
+    const busca = await connection.query(`SELECT * FROM customers`)
+    return res.send(busca.rows);
+} catch(err) {
+  return res.status(500).send(err);
 }
-try{
-    const insertion = await connection.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES($1, $2, $3, $4)`,[name, phone, cpf, birthday])
+}
+export async function getCustomersById(req, res){
+  const idP = req.params.id
+  console.log(idP.rows)
+  try{
+    const list = await connection.query("SELECT * FROM customers WHERE id = $1", [idP]);
+
+    res.send(list.rows[0])
+  }catch(err){
+    return res.status(500).send(err);
+  }
+}
+
+export async function insertCustomers(req, res) {
+  const { name, phone, cpf, birthday } = req.body;
+  console.log(typeof name);
+  console.log(typeof phone);
+  console.log(cpf.length);
+  const cpfExist = await connection.query(
+    `SELECT * FROM customers WHERE cpf='${cpf}'`
+  );
+  if (cpfExist.rows.length > 0) {
+    return res.sendStatus(409);
+  }
+  try {
+    const insertion = await connection.query(
+      `INSERT INTO customers (name, phone, cpf, birthday) VALUES($1, $2, $3, $4)`,
+      [name, phone, cpf, birthday]
+    );
     return res.sendStatus(200);
-}catch(err){
- return res.status(500).send(err)
-}
+  } catch (err) {
+    return res.status(500).send(err);
+  }
 }
--- src/routes/customers.js
+++ src/routes/customers.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import { customersSchenaValidation } from "../middlewares/customersSchenaValidation.js";
-import { insertCustomers } from "../controllers/customers.controller.js";
+import { insertCustomers, getCustomersCpf, getCustomersById } from "../controllers/customers.controller.js";
 
 const router = Router();
 
-router.get("/customer");
+router.get("/customer", getCustomersCpf);
+router.get("/customer/:id", getCustomersById)
 router.post("/customer", customersSchenaValidation, insertCustomers);
 
 export default router;
